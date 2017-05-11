a2637ea6ccec393ecae9bfc03f470e4f525aa6f1
carolakeskyla <carolakeskyla@hotmail.com>
2017-05-11 03:48:38 +0300

    Update FailiLugeja.java

--- src/FailiLugeja.java
+++ src/FailiLugeja.java
@@ -1,16 +1,15 @@
 package application;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
 public class FailiLugeja extends Main {
     public List<Aine> aineListike() {
-        List<Aine> ainelist = new ArrayList<>();
-        File fail = new File("ainefail.txt");
         try {
-            Scanner scanner = new Scanner(fail);
+            Scanner scanner = new Scanner(new FileReader("ainefail.txt"));
             while (scanner.hasNextLine()) {
                 String[] ainerida = scanner.nextLine().split(";");
                 ainelist.add(new Aine(Integer.parseInt(ainerida[0].replace(".", "")),
@@ -19,7 +18,7 @@ public class FailiLugeja extends Main {
             }
             scanner.close();
         } catch (FileNotFoundException e) {
-            
+
         }
         return ainelist;
     }
